69f2871483e992be5cc0bea703c7466ce20177e4
Alxander00 <alextejada025@gmail.com>
2026-03-03 12:30:45 -0600

    Subiendo proyecto inicial.

---
+++ sistema-clima/src/main/java/com/climatizacion/sistema_clima/SistemaClimaApplication.java
@@ -0,0 +1,13 @@
+package com.climatizacion.sistema_clima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClimaApplication.class, args);
+	}
+
+}
---
+++ sistema-clima/src/test/java/com/climatizacion/sistema_clima/SistemaClimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climatizacion.sistema_clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaClimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
