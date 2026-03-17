070e293fd5d494b1459244fd149432301ed433c4
Alvaro-GL-dev <alvaro.antonio.galan@gmail.com>
2026-03-17 11:21:10 -0600

    Implementar módulo de Servicio
    -Se agregó el CRUD y la lógica de negocio para la entidad Servicio.
    
    -Se incluyeron las capas de Controller, Service, Repository y DTO.

---
+++ sistema-clima/src/main/java/com/climatizacion/sistema_clima/service/ServicioService.java
@@ -0,0 +1,12 @@
+package com.climatizacion.sistema_clima.service;
+import com.climatizacion.sistema_clima.entities.ServicioEntity;
+
+import java.util.List;
+
+
+public interface ServicioService {
+    ServicioEntity guardar(ServicioEntity servicio);
+    List<ServicioEntity> listar();
+    ServicioEntity obtenerPorId(Integer id);
+    void eliminar(Integer id);
+}
