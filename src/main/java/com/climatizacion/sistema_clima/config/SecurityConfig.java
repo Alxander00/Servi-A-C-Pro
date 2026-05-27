ac5cd10c4db528ad1d13622b3b3965fafff99c52
Alxander00 <alextejada025@gmail.com>
2026-05-27 15:12:33 -0600

    Solucionando error Cors

--- src/main/java/com/climatizacion/sistema_clima/config/SecurityConfig.java
+++ src/main/java/com/climatizacion/sistema_clima/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500",
-                                "https://clinquant-tulumba-124b74.netlify.app/") // Direcciones de tu servidor frontend
+                                "https://clinquant-tulumba-124b74.netlify.app") // Direcciones de tu servidor frontend
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
