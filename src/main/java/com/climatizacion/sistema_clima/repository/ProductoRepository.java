67333a35876462954d3b5befba7cdab978825b4b
Alxander00 <alextejada025@gmail.com>
2026-05-27 14:01:48 -0600

    Solucionando error ProductoRepository

--- src/main/java/com/climatizacion/sistema_clima/repository/ProductoRepository.java
+++ src/main/java/com/climatizacion/sistema_clima/repository/ProductoRepository.java
@@ -12,7 +12,7 @@ public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> 
 
     List<ProductoEntity> findByActivoTrue();
 
-    List<ProductoEntity> findByCategoria(Long idCategoria);
+    List<ProductoEntity> findByCategoriaId(Long idCategoria);
 
     @Query("SELECT p, COALESCE(SUM(dp.cantidad), 0) as vendido " +
             "FROM ProductoEntity p LEFT JOIN DetallePedidoEntity dp ON dp.producto = p " +
