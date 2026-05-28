1507321bdee27dce9c890f2ea6fe10323e91966b
Alxander00 <alextejada025@gmail.com>
2026-05-28 02:32:07 -0600

    Cargar solicitudes

--- src/main/java/com/climatizacion/sistema_clima/repository/SolicitudServicioRepository.java
+++ src/main/java/com/climatizacion/sistema_clima/repository/SolicitudServicioRepository.java
@@ -2,10 +2,15 @@ package com.climatizacion.sistema_clima.repository;
 
 import com.climatizacion.sistema_clima.entities.SolicitudServicioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SolicitudServicioRepository extends JpaRepository<SolicitudServicioEntity, Long> {
-    List<SolicitudServicioEntity> findByEstadoOrderByFechaCreacionAsc(String estado);
+
+    @Query("SELECT s FROM SolicitudServicioEntity s JOIN FETCH s.cliente WHERE s.estado = :estado ORDER BY s.fechaCreacion ASC")
+    List<SolicitudServicioEntity> findByEstadoOrderByFechaCreacionAsc(@Param("estado") String estado);
+
     List<SolicitudServicioEntity> findByCliente_IdCliente(Long idCliente);
     long countByEstado(String estado);
 }
