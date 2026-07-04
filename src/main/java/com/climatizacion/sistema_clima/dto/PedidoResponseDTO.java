11c609ee21f527625ee9f873c919d76825c16e72
Alxander00 <alextejada025@gmail.com>
2026-07-04 14:58:36 -0600

    Paginacion en dashboard de admin

---
+++ src/main/java/com/climatizacion/sistema_clima/dto/PedidoResponseDTO.java
@@ -0,0 +1,24 @@
+package com.climatizacion.sistema_clima.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PedidoResponseDTO {
+    private Long idPedido;
+    private Long idUsuario;
+    private String nombreCliente;
+    private String fotoUrl;
+    private LocalDateTime fechaPedido;
+    private Double total;
+    private Boolean incluyeInstalacion;
+    private String estado;
+    private String direccion;
+}
